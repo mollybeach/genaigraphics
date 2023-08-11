42c2c144d99a6f1a3fd072febea5ea5e21691ba1
Molly Beach <mollyjbeach@gmail.com>
2023-08-11 10:24:45 -0700

    Add AssetLoader utility inside new utils folder in scripts
    - Created a new 'utils' folder within 'scripts' to house utility functionalities.
    - Introduced the `AssetLoader.ts` utility to handle dynamic loading of 3D assets, specifically GLTF and FBX formats.
    - Integrated with THREE's GLTFLoader and FBXLoader for asset parsing.
    - The utility supports dynamic extensions checks to determine which loader to use.
    - Designed to be extensible for future asset types

---
+++ src/scripts/utils/AssetLoader.ts
@@ -0,0 +1,30 @@
+import * as THREE from 'three';
+import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+//import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
+
+export type Assets = {
+  model?: { data?: THREE.Group, path: string },
+  texture?: { data?: GLTF, path: string },
+  lightTexture?: { data?: GLTF, path: string },
+  buttonTexture?: { data?: GLTF, path: string },
+};
+
+export async function loadAssets(assets: Assets) {
+  const gltfLoader = new GLTFLoader();
+  const fbxLoader = new FBXLoader();
+
+  for (const assetKey in assets) {
+    const asset = assets[assetKey as keyof Assets]!;
+    const ext = asset.path.split('.').pop();
+
+    if (ext === 'gltf') {
+      const object = await gltfLoader.loadAsync(asset.path);
+      asset.data = object;
+    } else if (ext === 'fbx') {
+      const object = await fbxLoader.loadAsync(asset.path);
+      asset.data = object;
+    }
+    // Add conditions for other asset types if needed
+  }
+}
