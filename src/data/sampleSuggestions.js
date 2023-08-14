0e479d9df723decb9bb8d59a4d4bacb49565e42a
Molly Beach <mollyjbeach@gmail.com>
2023-08-14 15:13:31 -0700

    Create sampleSuggestions.js

---
+++ src/data/sampleSuggestions.js
@@ -0,0 +1,12 @@
+export function getSampleSuggestionOptions() {
+    const sampleSuggestionsData =  [
+        {
+            "firstSuggestion": "Yes, I agree!",
+            "secondSuggestion": "No, I disagree.",
+            "thirdSuggestion": "Please provide more information."
+        }
+];
+    return sampleSuggestionsData;
+}
+
+
