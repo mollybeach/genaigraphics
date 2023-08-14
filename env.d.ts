d483badf6a6a8975c95aea4b933f37b0274f76ef
Molly Beach <mollyjbeach@gmail.com>
2023-08-14 14:52:08 -0700

    Update env.d.ts to include window.canvas variable
    Add zoom functionality to the WebGL canvas.

--- env.d.ts
+++ env.d.ts
@@ -1,10 +1,16 @@
 /// <reference types="astro/client" />
+// env.d.ts
 interface ImportMetaEnv {
-    readonly API_ENDPOINT: string;
-    readonly API_BEARER_TOKEN: string;
+    readonly PUBLIC_API_ENDPOINT: string;
+    readonly PUBLIC_API_BEARER_TOKEN: string;
+    readonly PUBLIC_AZURE_API_ENDPOINT: string;
     // more env variables...
-  }
+}
   
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+interface Window {
+  myCanvas: any; 
+}
+  
