9b7bdacf2ca6121e2a40a351bad38364ea1bb33d
Molly Beach <mollyjbeach@gmail.com>
2023-08-15 08:14:19 -0700

    Refactor: Update server.js to call import.meta.env directly within functions
    Updated server.js to call import.meta.env directly inside functions rather than assigning them to constants at the top.

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-//Server.js
+//path: server.js
 import axios from 'axios';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
+// Express setup
 const app = express();
 const port = 3001;
-
-const API_ENDPOINT = import.meta.env.PUBLIC_API_ENDPOINT;
-const API_BEARER_TOKEN = import.meta.env.PUBLIC_API_BEARER_TOKEN;
 
 // Middleware
 app.use(bodyParser.json());
@@ -16,23 +14,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({ origin: '*' }));
 app.use((req, res, next) => {
     console.log('Request Body:', req.body);
+    console.log('Response:', res);
     next();
 });
 
+// Helper function to make POST request to Azure ML API
 async function makePostRequest(body) {
     const headers = {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${API_BEARER_TOKEN}`
+        'Authorization': `Bearer ${import.meta.env.PUBLIC_API_BEARER_TOKEN}`
     };
 
     try {
-        return await axios.post(API_ENDPOINT, JSON.stringify(body), { headers });
+        return await axios.post(import.meta.env.PUBLIC_API_ENDPOINT, JSON.stringify(body), { headers });
     } catch (error) {
         console.error('Error in makePostRequest:', error);
         throw error;
     }
 }
-
+// POST route for Azure ML API
 app.post('/api', async (req, res) => {
     try {
         const responseData = await makePostRequest(req.body);
@@ -42,6 +42,7 @@ app.post('/api', async (req, res) => {
     }
 });
 
+// Listening for requests
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);  
 });
