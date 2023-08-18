b40d01b8678860fda00900de8ace66e64e26e8e9
Molly Beach <molly.beach@accenture.com>
2023-08-18 12:21:34 -0700

    Refactor graphics Updated CanvasScene to initialize ThreeCanvas directly from its script tag.
    - Createdgraphics folder and moved contents fromwebgl into it.
    - RenamedTCanvas.ts toThreeCanvas.ts and related files for better clarity.
    - Updated CanvasScene to initialize ThreeCanvas directly from its script tag.
    - Made relevant naming updates in various files for clearer context.
    - Updated project structure descriptions in both root and webapp READMEs.
    - Removed redundantcanvasSetup.ts andutils.ts files.
    - Update Astro configuration for environment variables.
    - Fix image paths in README.
    
      Updated project structure
    - Update environment variables to use PUBLIC_ prefix.
    - Add zoom functionality to the WebGL canvas.
    - Refactor buttons for page navigation.
    - Update chat application to dynamically adjust based on current page.
    - Add logging for debugging.
    - Modify API endpoint to fetch from environment variables.
    - Various UI improvements and minor bug fixes
    
    Refactor project structure and update configs
    - Move various config files (tailwind, prettier, astro, ts) into the config folder and create config.js.
    - Shift favicon.svg from public/images to public/images/svg.
    - Relocate env.d.ts and glsl.d.ts into the /types folder.
    - Transfer server.js into the server folder.
    - Update references in astro.config.mjs, server.js, and index.astro to match the new paths.
    
    Improve file structure and naming for clarity
    
    - Renamedentry.ts tocanvasSetup.ts for better clarity on its primary function.
    - Removed unnecessary script tags forcanvasSetup.ts in components.
    - Movedenv andenv.d.ts to the project root from thesrc directory.
    - Insrc/scripts/webgl/shader, renamedfs.glsl tofragmentShader.glsl andvs.glsl tovertexShader.glsl to provide a more descriptive naming convention.
    - Updated tsconfig to includeenv.d.ts at the root level.
    
     Update constants and improve naming convention for clarity.
    
    - ExtendpostAzureMLData to handle messages, suggestions, and animations.
    - Improve animation rendering with the ThreeCanvas library.
    - Centralize animations execution based on commands from AzureML response.
    - Update the store with new atoms, and integrate better with graphics module.
    - Refactor method names for clarity and specificity in the store.
    - Enhance API call responses handling with comprehensive error checks.
    - Migrate from sample data to dynamic data fetched from the API.

--- src/data/sampleMessagesData.js
+++ src/data/sampleMessagesData.js
@@ -3,7 +3,7 @@ export function getSampleMessagesData() {
       {
         "name": "AI Agent",
         "sender": "you",
-        "message": "Hello and welcome to Telehero Support. Whether you're seeking guidance on router set up, need technical assistance, or have general inquiries about our services, I'm here to help. How can I assist you today?",
+        "message": "Hello and welcome to jostaberry Support. Whether you're seeking guidance on router set up, need technical assistance, or have general inquiries about our services, I'm here to help. How can I assist you today?",
         "image": "https://cdn-icons-png.flaticon.com/512/2432/2432846.png",
         "timestamp": "10:00"
       }
