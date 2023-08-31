baa39ff787f9be456220d2569688c8b3b453b4c4
Molly Beach <mollyjbeach@gmail.com>
2023-08-31 11:00:27 -0700

    Create modeldataend.js

---
+++ modeldataend.js
@@ -0,0 +1,565 @@
+// path: webapp/src/data/modelData.js
+
+export function getModelData() {
+    const allModels = {
+      fbx: {
+        modelOriginal: {
+          animate: "spin",
+          data: {
+            scene: null,
+          },
+          path: "./models/fbx/model_Router.fbx",
+          type: "fbx",
+          cameraPosition: {
+            x: 0,
+            y: 0.15, 
+            z: 1,
+            fov: 50,
+          },
+        },
+      },
+      glb: {
+        modelCableTexture: {
+          animate: "spin",
+          data: {
+            scene: null,
+          },
+          path: "./models/glb/model_RouterCableTexture.glb",
+          type: "glb",
+          cameraPosition: {
+            x: 0,
+            y: 0.15, 
+            z: 1,
+            fov: 50,
+          },
+        },
+        modelLightTexture: {
+          animate: "spin",
+          data: {
+            scene: null,
+          },
+          path: "./models/glb/model_RouterLightBlinkTexture.glb",
+          type: "glb",
+          cameraPosition: {
+            x: 0,
+            y: 0.15, 
+            z: 1,
+            fov: 50,
+          },
+        },
+        modelResetButtonTexture: {
+          animate: "spin",
+          data: {
+            scene: null,
+          },
+          path: "./models/glb/model_RouterResetButtonTexture.glb",
+          type: "glb",
+          cameraPosition: {
+            x: 0,
+            y: 0.15, 
+            z: 1,
+            fov: 50,
+          },
+        },
+        modelRouter: {
+          animate: "spin",
+          data: {
+            scene: null,
+          },
+          path: "./models/glb/model_RouterCableTexture.glb",
+          type: "glb",
+          cameraPosition: {
+            x: 0,
+            y: 0.15, 
+            z: 1,
+            fov: 50,
+          },
+        },
+        model_Router90DegreeRotation: {
+          data: {
+            scene: null,
+          },
+          path: "./models/glb/model_Router90DegreeRotation.glb",
+          type: "glb",
+          cameraPosition: {
+            x: 0,
+            y: 0.15, 
+            z: 1,
+            fov: 50,
+          },
+        },
+        model_LogoAccenture: {
+          data: {
+            scene: null,
+          },
+          path: "./models/glb/model_LogoAccenture.glb",
+          type: "glb",
+          cameraPosition: {
+            x:  0,
+            y:  0,
+            z:  10,
+            fov: 50,
+          },
+        },
+      },
+      gltf: {
+        modelCableTexture: {
+          animate: "spin",
+          data: {
+            scene: null,
+          },
+          path: "./models/gltf/model_RouterCableTexture.gltf",
+          type: "gltf",
+          cameraPosition: {
+            x: 0,
+            y: 0.15, 
+            z: 1,
+            fov: 50,
+          },
+        },
+        modelLightTexture: {
+          animate: "spin",
+          data: {
+            scene: null,
+          },
+          path: "./models/gltf/model_RouterLightBlinkTexture.gltf",
+          type: "gltf",
+          cameraPosition: {
+            x: 0,
+            y: 0.15, 
+            z: 1,
+            fov: 50,
+          },
+        },
+        modelResetButtonTexture: {
+          data: {
+            scene: null,
+          },
+          path: "./models/gltf/model_RouterResetButtonTexture.gltf",
+          type: "gltf",
+          cameraPosition: {
+            x: 0,
+            y: 0.15, 
+            z: 1,
+            fov: 50,
+          },
+        },
+        modelRouter: {
+          animate: "spin",
+          data: {
+            scene: null,
+          },
+          path: "./models/gltf/model_RouterCableTexture.gltf",
+          type: "gltf",
+          cameraPosition: {
+            x: 0,
+            y: 0.15, 
+            z: 1,
+            fov: 50,
+          },
+        },
+      },
+      mp4: {
+        model_House1stHalf: {
+          data: {
+            duration: 0,
+            scene: null,
+          },
+          path: "./models/mp4/model_House1stHalf.mp4",
+          type: "mp4",
+          cameraPosition: {
+            x: 0,
+            y: 0, 
+            z: 0,
+            fov: 50,
+          },
+        },
+        model_House2ndHalf: {
+          data: {
+            duration: 0,
+            scene: null,
+          },
+          path: "./models/mp4/model_House2ndHalf.mp4",
+          type: "mp4",
+          cameraPosition: {
+            x: 0,
+            y: 0, 
+            z: 0,
+            fov: 50,
+          },
+        },
+        model_House: {
+          data: {
+            duration: 0,
+            scene: null,
+          },
+          path: "./models/mp4/model_House.mp4",
+          type: "mp4",
+          cameraPosition: {
+            x: 0,
+            y: 0, 
+            z: 0,
+            fov: 50,
+          },
+        },
+        model_HouseZoom: {
+          data: {
+            duration: 0,
+            scene: null,
+          },
+          path: "./models/mp4/model_HouseZoom.mp4",
+          type: "mp4",
+          cameraPosition: {
+            x: 0,
+            y: 0, 
+            z: 10,
+            fov: 50,
+          },
+        },
+        model_Upsell1st: {
+          data: {
+            duration: 0,
+            scene: null,
+          },
+          path: "./models/mp4/model_Upsell1st.mp4",
+          type: "mp4",
+          cameraPosition: {
+            x: 0,
+            y: 0, 
+            z: 10,
+            fov: 50,
+          },
+        },
+        model_Upsell2nd: {
+          data: {
+            duration: 0,
+            scene: null,
+          },
+          path: "./models/mp4/model_Upsell2nd.mp4",
+          type: "mp4",
+          cameraPosition: {
+            x: 0,
+            y: 0, 
+            z: 10,
+            fov: 50,
+          },
+        },
+        model_Upsell3rd: {
+          data: {
+            duration: 0,
+            scene: null,
+          },
+          path: "./models/mp4/model_Upsell3rd.mp4",
+          type: "mp4",
+          cameraPosition: {
+            x: 0,
+            y: 0, 
+            z: 10,
+            fov: 50,
+          },
+        },
+        model_Upsell4th: {
+          data: {
+            duration: 0,
+            scene: null,
+          },
+          path: "./models/mp4/model_Upsell4th.mp4",
+          type: "mp4",
+          cameraPosition: {
+            x: 0,
+            y: 0, 
+            z: 10,
+            fov: 50,
+          },
+        },
+        model_Upsell5th: {
+          data: {
+            duration: 0,
+            scene: null,
+          },
+          path: "./models/mp4/model_Upsell5th.mp4",
+          type: "mp4",
+          cameraPosition: {
+            x: 0,
+            y: 0, 
+            z: 10,
+            fov: 50,
+          },
+        },
+      },
+    };
+    return allModels;
+  }
+  
+// RETRIEVES MAPPED CANVAS ATTRIBUTES BASED RESPONSE FROM AZURE ML
+export function getMappedAttributes(response) {
+  const allModels = getModelData();
+  
+  const responseToDataMap = {
+      "cable": { 
+           command: "cable",
+          animate: allModels.glb.modelCableTexture.animate,
+          title: "Install Your Router",
+          path: allModels.glb.modelCableTexture.path,
+          data: allModels.glb.modelCableTexture.data,
+          type: allModels.glb.modelCableTexture.type,
+          cameraPosition: {
+            x: allModels.glb.modelCableTexture.cameraPosition.x,
+            y: allModels.glb.modelCableTexture.cameraPosition.y,
+            z: allModels.glb.modelCableTexture.cameraPosition.z,
+            fov: allModels.glb.modelCableTexture.cameraPosition.fov,
+          },
+      },
+      "cord": {
+          command: "cord",
+          animate: allModels.glb.modelCableTexture.animate,
+          title: "Verify Router Power Cord",
+          path: allModels.glb.modelCableTexture.path,
+          data: allModels.glb.modelCableTexture.data,
+          type: allModels.glb.modelCableTexture.type.replace,
+          cameraPosition: {
+            x: allModels.glb.modelCableTexture.cameraPosition.x,
+            y: allModels.glb.modelCableTexture.cameraPosition.y,
+            z: allModels.glb.modelCableTexture.cameraPosition.z,
+            fov: allModels.glb.modelCableTexture.cameraPosition.fov,
+          },
+      },
+      "house": {
+          command: "house",
+          title: "Verify Indoor Setup Location",
+          path: allModels.mp4.model_HouseZoom.path,
+          data: allModels.mp4.model_HouseZoom.data,
+          type: allModels.mp4.model_HouseZoom.type,
+          cameraPosition: {
+            x: allModels.mp4.model_HouseZoom.cameraPosition.x,
+            y: allModels.mp4.model_HouseZoom.cameraPosition.y,
+            z: allModels.mp4.model_HouseZoom.cameraPosition.z,
+            fov: allModels.mp4.model_HouseZoom.cameraPosition.fov,
+          },
+      },
+      "light": {
+          command: "light",
+          animate: allModels.glb.modelLightTexture.animate,
+          title: "Verify Router Lights",
+          path: allModels.glb.modelLightTexture.path,
+          data: allModels.glb.modelLightTexture.data,
+          type: allModels.glb.modelLightTexture.type,
+          cameraPosition: {
+            x: allModels.glb.modelLightTexture.cameraPosition.x,
+            y: allModels.glb.modelLightTexture.cameraPosition.y,
+            z: allModels.glb.modelLightTexture.cameraPosition.z,
+            fov: allModels.glb.modelLightTexture.cameraPosition.fov,
+          },
+      },
+      "reset": {
+          command: "reset",
+          animate: allModels.glb.modelResetButtonTexture.animate,
+          title: "Verify Router Reset Button",
+          path: allModels.glb.modelResetButtonTexture.path,
+          data: allModels.glb.modelResetButtonTexture.data,
+          type: allModels.glb.modelResetButtonTexture.type,
+          cameraPosition: {
+            x: allModels.glb.modelResetButtonTexture.cameraPosition.x,
+            y: allModels.glb.modelResetButtonTexture.cameraPosition.y,
+            z: allModels.glb.modelResetButtonTexture.cameraPosition.z,
+            fov: allModels.glb.modelResetButtonTexture.cameraPosition.fov,
+          },
+      },
+      "routerStatic": {
+          command: "routerStatic",
+          animate: allModels.glb.modelRouter.animate,
+          title: "Install Your Router",
+          path: allModels.glb.modelRouter.path,
+          data: allModels.glb.modelRouter.data,
+          type: allModels.glb.modelRouter.type,
+          cameraPosition: {
+            x: allModels.glb.modelRouter.cameraPosition.x,
+            y: allModels.glb.modelRouter.cameraPosition.y,
+            z: allModels.glb.modelRouter.cameraPosition.z,
+            fov: allModels.glb.modelRouter.cameraPosition.fov,
+          },
+      },
+      "upsell": {
+        command: "upsell",
+        assets:[
+           { 
+            command: "upsell1",
+            title: "Upsell 1st",
+            path: allModels.mp4.model_Upsell1st.path,
+            data: allModels.mp4.model_Upsell1st.data,
+            type: allModels.mp4.model_Upsell1st.type,
+            cameraPosition: {
+              x: allModels.mp4.model_Upsell1st.cameraPosition.x,
+              y: allModels.mp4.model_Upsell1st.cameraPosition.y,
+              z: allModels.mp4.model_Upsell1st.cameraPosition.z,
+              fov: allModels.mp4.model_Upsell1st.cameraPosition.fov,
+            },
+          },{  
+            command: "upsell2",
+            title: "Upsell 2nd",
+            path: allModels.mp4.model_Upsell2nd.path,
+            data: allModels.mp4.model_Upsell2nd.data,
+            type: allModels.mp4.model_Upsell2nd.type,
+            cameraPosition: {
+              x: allModels.mp4.model_Upsell2nd.cameraPosition.x,
+              y: allModels.mp4.model_Upsell2nd.cameraPosition.y,
+              z: allModels.mp4.model_Upsell2nd.cameraPosition.z,
+              fov: allModels.mp4.model_Upsell2nd.cameraPosition.fov,
+            },
+          },{ 
+            command: "upsell3",
+            title: "Upsell 3rd",
+            path: allModels.mp4.model_Upsell3rd.path,
+            data: allModels.mp4.model_Upsell3rd.data,
+            type: allModels.mp4.model_Upsell3rd.type,
+            cameraPosition: {
+              x: allModels.mp4.model_Upsell3rd.cameraPosition.x,
+              y: allModels.mp4.model_Upsell3rd.cameraPosition.y,
+              z: allModels.mp4.model_Upsell3rd.cameraPosition.z,
+              fov: allModels.mp4.model_Upsell3rd.cameraPosition.fov,
+            },
+          },{
+            command: "upsell4",
+            title: "Upsell 4th",
+            path: allModels.mp4.model_Upsell4th.path,
+            data: allModels.mp4.model_Upsell4th.data,
+            type: allModels.mp4.model_Upsell4th.type,
+            cameraPosition: {
+              x: allModels.mp4.model_Upsell4th.cameraPosition.x,
+              y: allModels.mp4.model_Upsell4th.cameraPosition.y,
+              z: allModels.mp4.model_Upsell4th.cameraPosition.z,
+              fov: allModels.mp4.model_Upsell4th.cameraPosition.fov,
+            },
+          },{
+            command: "upsell5",
+            title: "Upsell 5th",
+            path: allModels.mp4.model_Upsell5th.path,
+            data: allModels.mp4.model_Upsell5th.data,
+            type: allModels.mp4.model_Upsell5th.type,
+            cameraPosition: {
+              x: allModels.mp4.model_Upsell5th.cameraPosition.x,
+              y: allModels.mp4.model_Upsell5th.cameraPosition.y,
+              z: allModels.mp4.model_Upsell5th.cameraPosition.z,
+              fov: allModels.mp4.model_Upsell5th.cameraPosition.fov,
+            },
+         },
+        ],
+        data: {
+          scene: null,
+        },
+        title: "Upsell", 
+        type: "multipleMp4s",
+      },
+      "upsell1": {
+        command: "upsell1",
+        title: "Upsell 1st",
+        path: allModels.mp4.model_Upsell1st.path,
+        data: allModels.mp4.model_Upsell1st.data,
+        type: allModels.mp4.model_Upsell1st.type,
+        cameraPosition: {
+          x: allModels.mp4.model_Upsell1st.cameraPosition.x,
+          y: allModels.mp4.model_Upsell1st.cameraPosition.y,
+          z: allModels.mp4.model_Upsell1st.cameraPosition.z,
+          fov: allModels.mp4.model_Upsell1st.cameraPosition.fov,
+        },
+      },
+      "upsell2": {
+        command: "upsell2",
+        title: "Upsell 2nd",
+        path: allModels.mp4.model_Upsell2nd.path,
+        data: allModels.mp4.model_Upsell2nd.data,
+        type: allModels.mp4.model_Upsell2nd.type,
+        cameraPosition: {
+          x: allModels.mp4.model_Upsell2nd.cameraPosition.x,
+          y: allModels.mp4.model_Upsell2nd.cameraPosition.y,
+          z: allModels.mp4.model_Upsell2nd.cameraPosition.z,
+          fov: allModels.mp4.model_Upsell2nd.cameraPosition.fov,
+        },
+      },
+      "upsell3": {
+        command: "upsell3",
+        title: "Upsell 3rd",
+        path: allModels.mp4.model_Upsell3rd.path,
+        data: allModels.mp4.model_Upsell3rd.data,
+        type: allModels.mp4.model_Upsell3rd.type,
+        cameraPosition: {
+          x: allModels.mp4.model_Upsell3rd.cameraPosition.x,
+          y: allModels.mp4.model_Upsell3rd.cameraPosition.y,
+          z: allModels.mp4.model_Upsell3rd.cameraPosition.z,
+          fov: allModels.mp4.model_Upsell3rd.cameraPosition.fov,
+        },
+      },
+      "upsell4": {
+        command: "upsell4",
+        title: "Upsell 4th",
+        path: allModels.mp4.model_Upsell4th.path,
+        data: allModels.mp4.model_Upsell4th.data,
+        type: allModels.mp4.model_Upsell4th.type,
+        cameraPosition: {
+          x: allModels.mp4.model_Upsell4th.cameraPosition.x,
+          y: allModels.mp4.model_Upsell4th.cameraPosition.y,
+          z: allModels.mp4.model_Upsell4th.cameraPosition.z,
+          fov: allModels.mp4.model_Upsell4th.cameraPosition.fov,
+        },
+      },
+      "upsell5": {
+        command: "upsell5",
+        title: "Upsell 5th",
+        path: allModels.mp4.model_Upsell5th.path,
+        data: allModels.mp4.model_Upsell5th.data,
+        type: allModels.mp4.model_Upsell5th.type,
+        cameraPosition: {
+          x: allModels.mp4.model_Upsell5th.cameraPosition.x,
+          y: allModels.mp4.model_Upsell5th.cameraPosition.y,
+          z: allModels.mp4.model_Upsell5th.cameraPosition.z,
+          fov: allModels.mp4.model_Upsell5th.cameraPosition.fov,
+        },
+      },
+      "router": {
+          command: "router",
+          title: "Install Your Router",
+          path: allModels.glb.model_Router90DegreeRotation.path,
+          data: allModels.glb.model_Router90DegreeRotation.data,
+          type: allModels.glb.model_Router90DegreeRotation.type,
+          cameraPosition: {
+            x: allModels.glb.model_Router90DegreeRotation.cameraPosition.x,
+            y: allModels.glb.model_Router90DegreeRotation.cameraPosition.y,
+            z: allModels.glb.model_Router90DegreeRotation.cameraPosition.z,
+            fov: allModels.glb.model_Router90DegreeRotation.cameraPosition.fov,
+          },
+      },
+      default: {
+          command: "default",
+          title: "Welcome",
+          path: allModels.glb.model_LogoAccenture.path,
+          data: allModels.glb.model_LogoAccenture.data,
+          type: allModels.glb.model_LogoAccenture.type,
+          cameraPosition: {
+            x: allModels.glb.model_LogoAccenture.cameraPosition.x,
+            y: allModels.glb.model_LogoAccenture.cameraPosition.y,
+            z: allModels.glb.model_LogoAccenture.cameraPosition.z,
+            fov: allModels.glb.model_LogoAccenture.cameraPosition.fov,
+          }
+      }
+  };
+ 
+  const data = responseToDataMap[response] || responseToDataMap.default;
+  return data;
+}
+
+
+/*
+commands:
+glb: default, router, routerStatic, cable, cord, light, reset
+mp4: house, upsell
+*/
+
+//ASSIGN THE MODEL THAT RENDER ON START UP
+export const baseModel = "default";
+
+//MAPPED ATTRIBUTES FOR THE BASE MODEL
+export function getBaseModel() {
+  return getMappedAttributes(baseModel);
+}
+// REPRESENTS LIST OF MODELS CURRENTLY PRESENTED ON THE SCENE
+export const activeModels = [getBaseModel()];
