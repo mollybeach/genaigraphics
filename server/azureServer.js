711a5eb3a2b09fb56006ab7869f317e289b147b7
Molly Beach <mollyjbeach@gmail.com>
2023-09-01 14:20:08 -0700

    Create azureServer.js

---
+++ server/azureServer.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const { AppConfigurationClient } = require("@azure/app-configuration");
+const path = require('path');
+
+const app = express();
+const API_PORT = 3001; // or your desired port
+const appConfigEndpoint = "Endpoint=https://vz-prod-appconfig.azconfig.io;Id=aMah;Secret=gdgUR0rcREnaDEAS9WbGsPxOU/XnmWfIwxU=";
+const client = new AppConfigurationClient(appConfigEndpoint);
+
+// Serve static files from Astro's build directory
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.get('/api/getSecret/:key', async (req, res) => {
+    try {
+        const key = req.params.key;
+        console.log(key)
+        const setting = await client.getConfigurationSetting({ key });
+        res.json({ value: setting.value });
+    } catch (error) {
+        res.status(500).json({ error: "Failed to fetch secret" });
+    }
+});
+
+app.listen(API_PORT, () => {
+    console.log(`Server running at http://localhost:${API_PORT}/`);
+});
