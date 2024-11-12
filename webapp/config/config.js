aa2f5f22e11fbe01ea9e592c9bbc4790db141b5f
mollybeach <mollyjbeach@gmail.com>
2024-11-12 09:12:20 -0800

    fix: update asset paths for GitHub Pages

--- webapp/config/config.js
+++ webapp/config/config.js
@@ -1,5 +1,6 @@
 // path: webapp/config/config.js
-export const BASE_URL = '/';
+// export const BASE_URL = '/';
+export const BASE_URL = '/genaigraphics';
 export const PORT = 3000;
 export const API_PORT = 3001;
 export const WEBSITE_NAME = "Telehero";
